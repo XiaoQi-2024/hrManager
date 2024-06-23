431830995c70666c5fbbbf92a88c5e233a63005e
XiaoQi-2024 <lzwkt000@163.com>
2024-06-23 22:00:12 +0800

    组织架构树形结构

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,17 +1,40 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <el-tree default-expand-all :data="depts" :props="defaultProps" />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data() {
+    return {
+      depts: [{ name: '黑马', children: [{
+        name: '行政'
+      }, {
+        name: '人事'
+      }, {
+        name: 'IT',
+        children: [{
+          name: '前端'
+        }, {
+          name: '后端'
+        }]
+      }] }], // 数据属性
+      defaultProps: {
+        label: 'name', // 要显示的字段名name
+        children: 'children' // 读取子节点的字段名
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.app-container {
+  padding: 30px 40px;
+  font-size: 14px;
+}
 </style>
