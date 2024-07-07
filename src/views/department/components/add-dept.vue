41d8872b40f374360951a64f2bafc848e95e52df
XiaoQi-2024 <lzwkt000@163.com>
2024-07-07 12:27:28 +0800

    添加部门表单校验

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -1,18 +1,19 @@
 <template>
   <el-dialog title="新增部门" :visible="showDialog" @close="close">
     <!-- 表单内容 -->
-    <el-form label-position="right" label-width="100px">
-      <el-form-item label="部门名称">
-        <el-input style="width: 80%;" placeholder="1-10字符" size="mini" />
+    <el-form ref="addDept" :model="formData" :rules="rules" label-position="right" label-width="100px">
+      <el-form-item prop="name" label="部门名称">
+        <el-input v-model="formData.name" style="width: 80%;" placeholder="1-10字符" size="mini" />
       </el-form-item>
-      <el-form-item label="部门编号">
-        <el-input style="width: 80%;" placeholder="1-10字符" size="mini" />
+      <el-form-item prop="code" label="部门编号">
+        <el-input v-model="formData.code" style="width: 80%;" placeholder="1-10字符" size="mini" />
       </el-form-item>
-      <el-form-item label="部门负责人">
-        <el-select style="width: 80%;" placeholder="请选择部门负责人" size="mini" />
+      <el-form-item prop="managerId" label="部门负责人">
+        <el-select v-model="formData.managerId" style="width: 80%;" placeholder="请选择部门负责人" size="mini" />
       </el-form-item>
-      <el-form-item label="部门介绍">
-        <el-input type="textarea" rows="4" style="width: 80%;" placeholder="1-100字符" size="mini" />
+      <el-form-item prop="introduce" label="部门介绍">
+        <el-input v-model="formData.introduce" type="textarea" rows="4" style="width: 80%;" placeholder="1-100字符"
+          size="mini" />
       </el-form-item>
       <el-form-item>
         <!-- 按钮 -->
@@ -28,6 +29,8 @@
 </template>
 
 <script>
+import { getDepartmentInfo } from '@/api/department'
+
 export default {
   name: 'AddDept',
   props: {
@@ -36,8 +39,57 @@ export default {
       default: false
     }
   },
+  data() {
+    return {
+      formData: {
+        code: '', // 部门编码
+        introduce: '', // 部门介绍
+        managerId: '', // 部门负责人Id
+        name: '', // 部门名称
+        pid: '' // 父级部门Id
+      },
+      rules: {
+        code: [
+          { required: true, message: '部门编码不能为空', trigger: 'blur' },
+          { min: 2, max: 10, message: '部门编码长度2-10个字符', trigger: 'blur' },
+          {
+            trigger: 'blur',
+            validator: async (rule, vlue, callback) => {
+              const result = await getDepartmentInfo()
+              if (result.some(item => item.code === vlue)) {
+                callback(new Error('部门编码已存在'))
+              } else {
+                callback()
+              }
+            }
+          }
+        ], // 部门编码
+        introduce: [
+          { required: true, message: '部门介绍不能为空', trigger: 'blur' },
+          { min: 1, max: 100, message: '部门介绍长度2-10个字符', trigger: 'blur' }
+        ], // 部门介绍
+        managerId: [{ required: true, message: '部门负责人不能为空', trigger: 'blur' }], // 部门负责人Id
+        name: [
+          { required: true, message: '部门名称不能为空', trigger: 'blur' },
+          { min: 2, max: 10, message: '部门名称长度2-10个字符', trigger: 'blur' },
+          {
+            trigger: 'blur',
+            validator: async (rule, vlue, callback) => {
+              const result = await getDepartmentInfo()
+              if (result.some(item => item.name === vlue)) {
+                callback(new Error('部门名称已存在'))
+              } else {
+                callback()
+              }
+            }
+          }
+        ] // 部门名称
+      }
+    }
+  },
   methods: {
     close() {
+      // this.$refs.formData.resetFields() // 重置表单数据和校验规则
       // 修改父组件的值 子传父 自定义事件update:showDialog
       this.$emit('update:showDialog', false)
     }
