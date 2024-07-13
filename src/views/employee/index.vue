71a933f2828e0b9eb3716883b916187fe0e4b290
XiaoQi-2024 <lzwkt000@163.com>
2024-07-13 13:16:11 +0800

    树形结构默认选择第一项

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -5,11 +5,14 @@
         <el-input style="margin-bottom: 10px;" type="text" prefix-icon="el-icon-search" placeholder="输入员工姓名全员搜索" />
         <!-- 树形组件 -->
         <el-tree
+          ref="deptTree"
+          node-key="id"
           :data="depts"
           :props="defaultProps"
           default-expand-all
           :expand-on-click-node="false"
           highlight-current
+          @current-change="changeNode"
         />
       </div>
       <div class="right">
@@ -36,6 +39,10 @@ export default {
       defaultProps: {
         children: 'children', // depts树形结构的子节点属性
         label: 'name' // depts树形结构要显示的字段
+      },
+      // 存储查询参数
+      queryParams: {
+        departmentId: null
       }
     }
   },
@@ -46,6 +53,15 @@ export default {
     async getDepartmentInfo() {
       // let result = await getDepartmentInfo()
       this.depts = transListToTreeData(await getDepartmentInfo(), 0)
+      this.queryParams.departmentId = this.depts[0].id
+      // 树形结构的页面渲染是异步的，设置选中节点需要等待页面渲染完成，使用$nextTick方法等待异步完成
+      this.$nextTick(() => {
+        // 调用树形组件的方法：设置选中节点
+        this.$refs.deptTree.setCurrentKey(this.queryParams.departmentId)
+      })
+    },
+    changeNode(node) {
+      this.queryParams.departmentId = node.id
     }
   }
 }
